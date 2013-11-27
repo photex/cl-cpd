9f129bb623b36f80d2f496b3ebe2671604692cb2
Chip <photex@gmail.com>
2013-11-27 11:11:03 -0800

    Okie dokie, reading and writing are now complete

--- cpd.lisp
+++ cpd.lisp
@@ -25,6 +25,14 @@
      ("mirror" :mirror)
      ("default" :default)))
 
+(defmacro extend-region-type-name (region-type)
+  `(optima:match ,region-type
+     (:hold "hold")
+     (:slope "slope")
+     (:cycle "cycle")
+     (:mirror "mirror")
+     (:default "default")))
+
 (defmacro parse-track-line (line)
   `(ppcre:register-groups-bind (attr value) (+attr-regex+ ,line)
      (optima:match attr
@@ -60,8 +68,7 @@
     (next-line lines '(:tracks ()))))
 
 (defun from-file (filename)
-  (with-open-file (in filename
-                      :direction :input)
+  (with-open-file (in filename :direction :input)
     ;; Valid clip files surround their entire contents with { and } on their own line
     (unless (string= "{" (read-line in nil))
       (error (format nil "~a is not a valid clip file." filename)))
@@ -72,4 +79,34 @@
         collect (if line (string-trim '(#\space) line)
                     (error "Unexpected end of file!"))))))
 
-(defun to-file () nil)
+(defun to-file (clip filename &key (padding "  "))
+  (labels ((write-track (track out)
+             (let ((name (getf track :name))
+                   (data (getf track :data))
+                   (left-type (getf track :left-type))
+                   (right-type (getf track :right-type))
+                   (default (getf track :default)))
+               (format out "~a{~%" padding)
+               (format out "~a~aname = ~a~%" padding padding name)
+               (format out "~a~adata = " padding padding)
+               (loop for i across data
+                  do (format out "~a " i))
+               (format out "~%")
+               (when left-type
+                 (format out "~a~alefttype = ~a" padding padding (extend-region-type-name left-type)))
+               (when right-type
+                 (format out "~a~arighttype = ~a" padding padding (extend-region-type-name right-type)))
+               (when default
+                 (format out "~a~adefault = ~a" padding padding default))
+               (format out "~a}~%" padding))))
+    (let ((rate (format nil "rate = ~a" (getf clip :rate)))
+          (start (format nil "start = ~a" (getf clip :start)))
+          (track-length (format nil "tracklength = ~a" (getf clip :track-length)))
+          (track-count (format nil "tracks = ~a" (getf clip :track-count))))
+      (with-open-file (out filename :direction :output)
+        (format out "{~%")
+        (loop for item in (list rate start track-length track-count)
+           do (format out "~a~a~%" padding item))
+        (loop for track in (getf clip :tracks)
+           do (write-track track out))
+        (format out "}")))))
